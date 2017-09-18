ab71550b78bb10ba792d9c6fe641d1f5b8cefbf2
Gerson Janhuel <gersonjanhuel@gmail.ocm>
2017-09-18 11:54:34 +0700

    project init…

---
+++ Podfile
@@ -0,0 +1,7 @@
+source 'https://github.com/CocoaPods/Specs.git'
+use_frameworks!
+target 'SingaporePSI' do
+pod 'GoogleMaps'
+pod 'SwiftyJSON'
+pod 'Alamofire'
+end
---
+++ SingaporePSI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SingaporePSI
+//
+//  Created by Gerson Janhuel on 9/18/17.
+//  Copyright © 2017 Gerson Janhuel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
