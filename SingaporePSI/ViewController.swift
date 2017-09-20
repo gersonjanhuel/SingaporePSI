8db70342665dda037591e4f165fa4217d16b191c
Gerson Janhuel <gersonjanhuel@gmail.ocm>
2017-09-20 11:50:14 +0700

    initial map

--- SingaporePSI/ViewController.swift
+++ SingaporePSI/ViewController.swift
@@ -7,15 +7,27 @@
 //
 
 import UIKit
+import GoogleMaps
 
 class ViewController: UIViewController {
 
+    var mapView:GMSMapView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.showMap()
     }
 
-
-
+    func showMap() {
+        GMSServices.provideAPIKey(Constants.GoogleMapAPI.key)
+        
+        let camera = GMSCameraPosition.camera(withLatitude: 1.35735, longitude: 103.82, zoom: 10.5) //center of singapore
+        
+        self.mapView = GMSMapView.map(withFrame: self.view.frame, camera: camera)
+        
+        self.view = self.mapView
+    }
+    
 }
 
