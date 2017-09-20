7df4dfb9fca908cd0a0f55f659cc1181f5cb245f
Gerson Janhuel <gersonjanhuel@gmail.ocm>
2017-09-20 11:48:36 +0700

    set up Global Constants

---
+++ SingaporePSI/Constants.swift
@@ -0,0 +1,22 @@
+//
+//  Constants.swift
+//  SingaporePSI
+//
+//  Created by Gerson Janhuel on 9/20/17.
+//  Copyright © 2017 Gerson Janhuel. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    
+    struct ServiceAPI {
+        static let url = "https://api.data.gov.sg/v1"
+        static let headers = ["api-key": "4JXRLf8xvJ05vo42M4JUM9dQKky3tATR"]
+    }
+    
+    struct GoogleMapAPI {
+        static let key = "AIzaSyAVhx9JfocUamLXRBeBY0mia2R5G5MfyNk"
+    }
+    
+}
